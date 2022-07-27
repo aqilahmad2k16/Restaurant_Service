99f946bfbb924785e9564bf2aa80b752923c512a
Aqil Ahmad <er.aqilahmad031@gmail.com>
2022-07-27 21:39:48 +0530

    new commmit

---
+++ src/main/java/com/restaurantService/model/entity/RestaurantEntity.java
@@ -0,0 +1,13 @@
+package com.restaurantService.model.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name="restaurant")
+public class RestaurantEntity {
+    
+}
